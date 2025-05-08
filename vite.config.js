c650d1eb0566470bc6942a90087d97e1353a59c9
Direwen <direwensik@gmail.com>
2025-05-08 15:00:06 +0630

    Added Docker file and Modified Vite Confi

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,15 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    port: 3000,  // Set Vite to run on port 3000
+    host: '0.0.0.0',  // Allow external connections (required for Docker)
+    proxy: {
+      '/api': {
+        target: 'http://backend:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
